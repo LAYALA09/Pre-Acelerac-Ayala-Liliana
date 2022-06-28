d78933a54596dec42e028f64d23f1a0396943373
LAYALA09 <ayalilian2018@gmail.com>
2022-06-28 20:31:52 -0300

    Challenge Alkemy - Disney

--- src/main/java/com/alkemy/disney/disney/auth/service/UserDetailsCustomService.java
+++ src/main/java/com/alkemy/disney/disney/auth/service/UserDetailsCustomService.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import java.util.Collections;
 
+
 @Service
 public class UserDetailsCustomService implements UserDetailsService{
 
